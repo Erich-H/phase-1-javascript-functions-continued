d868131fc0ca3cb6478eab5a4065ca0edbe983f3
Erich <eheiseler@gmail.com>
2022-02-22 20:55:18 -0500

    commit tree 16646e80a652994e1f823a72b016ec4169a5c4e0

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+//function declaration with default params
+function saturdayFun(activity = "roller-skate") {
+    const satFun = `This Saturday, I want to ${activity}!`
+    return satFun
+}
+
+//function expression
+const mondayWork = function(activity = "go to the office") {
+    const work = `This Monday, I will ${activity}.`
+    return work
+}
+
+function wrapAdjective(flair = "*") {
+    return function(param1 = "special") {
+        return `You are ${flair}${param1}${flair}!`
+    }   
+}
